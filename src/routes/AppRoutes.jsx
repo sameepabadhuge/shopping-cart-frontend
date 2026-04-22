c8d36a7770e9f38b18dc605519edd2625e6f3c64
sameepabadhuge <sithusameepa@gmail.com>
2026-04-22 13:06:20 +0530

    create an update admin dashboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,29 +6,52 @@ import Home from "../pages/user/Home";
 
 import AdminLogin from "../pages/admin/AdminLogin";
 import Dashboard from "../pages/admin/Dashboard";
+import ManageProducts from "../pages/admin/ManageProducts";
+import ManageCategories from "../pages/admin/ManageCategories";
+
+import AdminLayout from "../layouts/AdminLayout";
 
 export default function AppRoutes() {
   return (
     <Routes>
 
       {/* Default */}
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route
+        path="/"
+        element={<Navigate to="/login" />}
+      />
 
-      {/* User Pages */}
+      {/* User */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
 
-      {/* Admin */}
-      <Route path="/admin/login" element={<AdminLogin />} />
-      <Route path="/admin/dashboard" element={<Dashboard />} />
+      {/* Admin Login */}
+      <Route
+        path="/admin/login"
+        element={<AdminLogin />}
+      />
 
+      {/* Admin Pages */}
+      <Route
+        path="/admin"
+        element={<AdminLayout />}
+      >
+        <Route
+          path="dashboard"
+          element={<Dashboard />}
+        />
 
-      {/* 404 */}
-      <Route path="*" element={<h1 className="text-center mt-10 text-2xl">Page Not Found</h1>} />
+        <Route
+          path="products"
+          element={<ManageProducts />}
+        />
 
-
-
+        <Route
+          path="categories"
+          element={<ManageCategories />}
+        />
+      </Route>
 
     </Routes>
   );
